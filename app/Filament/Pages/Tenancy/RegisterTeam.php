61fa836b6ae528bc1e39ee307d49b0843efb5224
Lalhmangaihenga <valpuia604@gmail.com>
2023-10-09 10:04:56 +0700

    admin create team for others user

--- app/Filament/Pages/Tenancy/RegisterTeam.php
+++ app/Filament/Pages/Tenancy/RegisterTeam.php
@@ -3,9 +3,12 @@
 namespace App\Filament\Pages\Tenancy;
 
 use App\Models\Team;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Forms\Set;
+use Filament\Notifications\Notification;
 use Filament\Pages\Tenancy\RegisterTenant;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
@@ -17,12 +20,18 @@ class RegisterTeam extends RegisterTenant
         return 'Register team';
     }
 
+    public static function getSlug(): string
+    {
+        return 'new-team';
+    }
+
     public function form(Form $form): Form
     {
         return $form
             ->schema([
                 TextInput::make('name')
                     ->unique()
+                    ->required()
                     ->live(onBlur: true)
                     ->afterStateUpdated(fn (Set $set, ?string $state) => $set('slug', Str::slug($state))),
 
@@ -30,15 +39,49 @@ class RegisterTeam extends RegisterTenant
                     ->unique()
                     ->disabled()
                     ->dehydrated(),
+
+                Select::make('members')
+                    ->required()
+                    ->relationship(name: 'members', titleAttribute: 'name')
+                    ->searchable()
+                    ->preload()
+                    ->createOptionForm([
+                        TextInput::make('name')
+                            ->required(),
+
+                        TextInput::make('email')
+                            ->unique()
+                            ->required()
+                            ->email(),
+
+                        TextInput::make('password')
+                            ->required()
+                            ->password(),
+
+                        Checkbox::make('is_team_owner'),
+                    ]),
             ]);
     }
 
     protected function handleRegistration(array $data): Model
     {
-        $team = Team::create($data);
+        $team = Team::create([
+            'name' => $data['name'],
+            'slug' => $data['slug'],
+        ]);
 
-        $team->members()->attach(auth()->user());
+        $team->members()->attach($data['members']);
+
+        Notification::make()
+            ->title('New team '.$team->name.' created successfully')
+            ->success()
+            ->send();
 
         return $team;
+    }
+
+    protected function getRedirectUrl(): ?string
+    {
+        return route('filament.admin.tenant');
     }
 }
